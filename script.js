4dfba6df1873e0258c102eb1ee2ed17313792d97
Vladimir-cloudj <73839515+Vladimir-cloudj@users.noreply.github.com>
2026-02-12 11:57:13 +0400

    feat: lesson01

---
+++ script.js
@@ -0,0 +1,12 @@
+const title = "сайт-визитка"
+const screens = "Простые, Сложные, Интерактивные"
+const screenPrice = 100
+const rollback = 15
+const fullPrice = 115
+const adaptive = true
+
+alert("Привет!")
+
+console.log(
+  `мы создаем "${title}", с поддержкой следующих типов экранов "${screens}". Цена проекта начинается от ${screenPrice}. На "налоги" уйдет ${screenPrice}, полная цена проекта - ${fullPrice}, адаптив "${adaptive}"`
+);
