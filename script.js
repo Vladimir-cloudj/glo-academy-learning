efc7d7b03d871e8efe4bf37780633347c91ea0a6
Vladimir-cloudj <73839515+Vladimir-cloudj@users.noreply.github.com>
2026-04-02 13:11:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+class First {
+    hello() {
+        console.log("Привет я метод родителя")
+    }
+}
+
+class Second extends First {
+    hello() {
+        super.hello()
+        console.log("А я наследуемый метод!");
+    }
+}
+
+const obj = new Second();
+obj.hello();
